cd2a740cd49ffb4763a3229b12a931af40f39946
VenusMeow <32319682+VenusMeow@users.noreply.github.com>
2018-10-31 17:34:21 -0400

    finally

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,13 +25,15 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-
     @user = User.new(user_params)
     if @user.save
       flash[:success] = 'Sign up successfully'
-      render 'sessions/new'
+      render root_url
     else
+      #params.delete :user
+      params.delete :owner
       render 'new'
+      #redirect_to new_user_path, alert: 'You have an error in your submission.'
     end
   end
 
@@ -67,7 +69,15 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
+      byebug
+      if(params.has_key?(:user))
         params.require(:user).permit(:name, :email, :address, :phone, :type, :password, :password_confirmation)
+      elsif (params.has_key?(:owner))
+        params.require(:owner).permit(:name, :email, :address, :phone, :type, :password, :password_confirmation)
+      else
+        params.require(:fixer).permit(:name, :email, :address, :phone, :type, :password, :password_confirmation)
+      end
+        #params.permit(:name, :email, :address, :phone, :type, :password, :password_confirmation, :user)
         # params.require(params[:type].to_sym).permit(:name, :email, :address, :phone, :type, :password, :password_confirmation)
         # key = (params.keys & %w(owner fixer))[0]
         # params.require(key).permit(:name, :email, :address, :phone, :type, :password, :password_confirmation)
