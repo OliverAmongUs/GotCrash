5844da4f88f260ae9f65056891de9a072f982279
Ti <zttttty@TideMacBook-Air.local>
2018-11-20 11:16:01 -0500

    some views

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,7 +20,30 @@ class ProfilesController < ApplicationController
   end
 
   def update
-    puts "123"
-    # current_user.update()
+    @profile = current_user
+    if @profile.update_attributes(user_params)
+      flash[:success] = 'Update successfully'
+      redirect_to @profile
+    else
+      render 'edit'
+    end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      if(params.has_key?(:user))
+        params.require(:user).permit(:name, :email, :address, :phone, :type, :password, :password_confirmation, :picture_url, :picture_url_cache)
+      elsif (params.has_key?(:owner))
+        params.require(:owner).permit(:name, :email, :address, :phone, :type, :password, :password_confirmation, :picture_url, :picture_url_cache)
+      else
+        params.require(:fixer).permit(:name, :email, :address, :phone, :type, :password, :password_confirmation, :picture_url, :picture_url_cache)
+      end
+
+    end
 end
