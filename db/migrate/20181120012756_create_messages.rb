161bd556107c2da4267f730b479054ae06543122
Elysian1196 <mdowns2015@brandeis.edu>
2018-11-24 16:53:04 -0800

    added file submission to messages

--- db/migrate/20181120012756_create_messages.rb
+++ db/migrate/20181120012756_create_messages.rb
@@ -1,9 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
+    drop_table :messages
     create_table :messages do |t|
       t.string :body
       t.integer :user_id
       t.integer :bid_id
+      t.string :picture_url
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2018_11_20_012756) do
-=======
-ActiveRecord::Schema.define(version: 2018_11_15_063009) do
->>>>>>> c7702887c17d6ca4a7a4fd04ac675ff9fe1d322f
 
   create_table "bids", force: :cascade do |t|
     t.integer "fixer_id"
@@ -44,7 +40,8 @@ ActiveRecord::Schema.define(version: 2018_11_15_063009) do
   create_table "messages", force: :cascade do |t|
     t.string "body"
     t.integer "user_id"
-    t.integer "conversation_id"
+    t.integer "bid_id"
+    t.string "picture_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
