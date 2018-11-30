b5e4209349525a5be3c170d0a4fdc0397474ed5c
Elysian1196 <mdowns2015@brandeis.edu>
2018-11-30 12:50:58 -0800

    updating phone number stuff

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,6 @@ class MessagesController < ApplicationController
   before_action do
    @bid = Bid.find(params[:bid_id])
    @fixer = User.find(@bid.fixer_id)
-   byebug
   end
 
   # GET /messages
@@ -35,7 +34,12 @@ class MessagesController < ApplicationController
   def create
     @message = @bid.messages.new(message_params)
     if @message.save
-      send_message()
+      if current_user.type == "Owner"
+        @receiver_number = @bid.fixer.phone
+      else
+        @receiver_number = @bid.owner.phone
+      end
+      send_message(@receiver_number)
       redirect_to fixer_bid_messages_path(@fixer, @bid)
     end
   end
@@ -71,21 +75,15 @@ class MessagesController < ApplicationController
       params.require(:message).permit(:body, :user_id, :picture_url, :picture_url_cache)
     end
 
-    def send_message()
+    def send_message(phone)
       @alert_message = "You have an awaiting message on GotCrash!"
       @twilio_number = ENV['TWILIO_NUMBER']
-      if current_user.type == "Owner"
-        @receiver_number = @bid.fixer.phone
-      else
-        @receiver_number = @bid.owner.phone
-      end
       @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 
       message = @client.messages.create(
         :from => @twilio_number,
-        :to => "+1#{@receiver_number}",
+        :to => "+1#{phone}",
         :body => @alert_message
-        #Venus # 7813928999
         # US phone numbers can make use of an image as well.
         # :media_url => image_url
       )
