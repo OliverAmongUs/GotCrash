0dad93c0fe3f454509990b9fe78aa93a0e0a2365
VenusMeow <32319682+VenusMeow@users.noreply.github.com>
2018-12-04 12:21:53 -0500

    add labels when choosing reports

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2018_12_03_060404) do
     t.string "body"
     t.integer "user_id"
     t.integer "bid_id"
+    t.string "picture_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
