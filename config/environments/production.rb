2d87aaf4e280a9361561755b22a46b36ef7303a8
Elysian1196 <mdowns2015@brandeis.edu>
2018-12-07 22:11:15 -0800

    trying again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   #Action Cable Heroku Compatability
-  config.middleware.use RoomActionCable
+  config.middleware.use "RoomActionCable"
   config.web_socket_server_url = "wss://got-crash.herokuapp.com/"
 
   # Code is not reloaded between requests.
