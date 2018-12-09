bea5b548b3e4b5af96d675228a30ec6e5c777480
Oliver Wen <wb493797144@gmail.com>
2018-12-09 17:24:58 -0500

    try to fix bug

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -43,10 +43,10 @@ class ReviewsController < ApplicationController
 
     if @review.save
       flash[:success] = 'create new review successfully'
-      updateFixer = User.find(@review.fixer_id)
+      # updateFixer = User.find(@review.fixer_id)
+      # # byebug
+      # numReview = Review.where(:fixer_id @review.fixer_id)
       # byebug
-      numReview = Review.where(:fixer_id @review.fixer_id)
-      byebug
       # updateFixer.average_rating = (updateFixer.average_rating * numReview + @review.rating) / (numReview + 1)
       redirect_to @review
     else
