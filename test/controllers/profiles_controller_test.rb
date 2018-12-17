18888310d593891c2a874e2b7700856dca1cb1d4
Elysian1196 <mdowns2015@brandeis.edu>
2018-12-17 13:37:24 -0800

    fixed a bunch more tests

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,13 +1,22 @@
 require 'test_helper'
 
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @fixer = users(:two) #test Fixer
+    sign_in
+  end
+
+  def sign_in
+    post login_path, params: { session: { email:    @fixer.email,
+                                          password: 'password' } }
+  end
   test "should get edit" do
     get profiles_edit_url
     assert_response :success
   end
 
   test "should get show" do
-    get profiles_show_url
+    get profiles_path
     assert_response :success
   end
 
